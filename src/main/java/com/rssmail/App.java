b109991b3cad87cc724b036750d494c81749ad6a
Steffen Mads Lyng <sm@lyng.dev>
2021-12-11 23:53:06 +0100

    named to App

--- src/main/java/com/rssmail/App.java
+++ src/main/java/com/rssmail/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RssmailApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RssmailApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
