d1c75c301e49a386812fafb57c87f10cc94e7f46
Steffen Mads Lyng <sm@lyng.dev>
2021-12-11 23:01:21 +0100

    initial commit, based on Spring Initializer with Spring Boot and Spring Web

---
+++ src/main/java/com/rssmail/RssmailApplication.java
@@ -0,0 +1,13 @@
+package com.rssmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rssmail/RssmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rssmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
