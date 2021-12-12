136e994b039ad152e4ae380c528138aa45075751
Steffen Mads Lyng <sm@lyng.dev>
2021-12-12 06:16:33 +0100

    added test of health controller

---
+++ src/test/java/com/rssmail/controllers/HealthControllerTests.java
@@ -0,0 +1,36 @@
+package com.rssmail.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
+@ExtendWith({
+  SpringExtension.class, 
+  MockitoExtension.class
+})
+public class HealthControllerTests {
+
+  @Autowired
+  private MockMvc mvc;
+
+	@Test
+	void testHealthEndpoint() throws Exception {
+    this.mvc.perform(get("/health"))
+      .andExpect(status().isOk());
+    
+	}
+
+}
