ce9279905c373422ac1f5c2fad89e0d042d3ace8
Steffen Mads Lyng <sm@lyng.dev>
2021-12-15 08:34:47 +0100

    build script now takes parameter for finalname and passes it along to docker

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,13 @@
 set -x
 set -e
 
-docker build . -f docker/Dockerfile -t rssmail
+if (( $# < 1 ))
+then
+    printf "%b" "Error. Not enough arguments.\n" >&2
+    printf "%b" "usage: $0 <finalName>\n" >&2
+    exit 1
+fi
+
+FINAL_NAME=$1
+
+docker build . -f docker/Dockerfile -t rssmail --build-arg finalName=$FINAL_NAME
