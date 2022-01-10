74ee073d9c2be29eec0ff7263673e2d7ed787aa4
Steffen Mads Lyng <sm@lyng.dev>
2022-01-10 21:56:08 +0100

    chore: housekeeping

--- src/main/java/com/rssmail/App.java
+++ src/main/java/com/rssmail/App.java
@@ -1,28 +1,16 @@
 package com.rssmail;
 
 import com.rssmail.scheduler.RssMailScheduler;
-import com.rssmail.services.RssService.RssService;
-import com.rssmail.services.SubscriptionService.AwsSubscriptionService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import java.util.Arrays;
-import java.util.Map;
 
 @SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {
 		final ApplicationContext appContext = SpringApplication.run(App.class, args);
-		// String[] beanNames = appContext.getBeanDefinitionNames();
-
-		// for (String beanName : beanNames) {
-		// 	System.out.println(beanName + " : " + appContext.getBean(beanName).getClass().toString());
-		// }
 
 		try {
 			var rssScheduler = (RssMailScheduler)appContext.getBean("rssMailScheduler");
@@ -30,17 +18,5 @@ public class App {
 		} catch (Exception e) {
 			System.out.println("something failed");
 		}
-
-		// try {
-		// 	var rss = (RssService)appContext.getBean("rssService");
-		// 	rss.read();
-		// } catch (Exception e) {
-		// 	System.out.println("something failed");
-		// }
-
-		// var subscriptionService = (AwsSubscriptionService)appContext.getBean("awsSubscriptionService");
-		// var subscriptionId = subscriptionService.createSubscription("http://google.com", "s@sunlyng.dk");
-		// subscriptionService.validateSubscription(subscriptionId,"abc");
-		// subscriptionService.deleteSubscription(subscriptionId,"s@sunlyng");
 	}
 }
