1ac9c4984af47435426f9cc9109f0414f598f230
Steffen Mads Lyng <sm@lyng.dev>
2022-02-08 21:51:05 +0100

    bug: can now download dependencies, i accidentally deleted the egress rule. Also restructured logs a bit

--- infrastructure/modules/backend/main.tf
+++ infrastructure/modules/backend/main.tf
@@ -76,4 +76,12 @@ resource "aws_security_group" "allow-ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  egress {
+    description      = "allow all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
--- infrastructure/modules/backend/userscript.sh
+++ infrastructure/modules/backend/userscript.sh
@@ -5,31 +5,32 @@ LOG=/etc/user-data.log
 function install_dependencies() {
 
   echo "Installing aws CLI" >> $LOG
-  yum install awscli jq -y
+  yum install awscli jq -y >> $LOG
   echo "Installing docker with amazon-linux-extras" >> $LOG
-  amazon-linux-extras install docker -y && \ 
+  amazon-linux-extras install docker -y >> $LOG
   echo "Starting docker service" >> $LOG
-  service docker start && \ 
+  service docker start >> $LOG
   echo "Setting usermod for docker" >> $LOG
-  usermod -a -G docker ec2-user
+  usermod -a -G docker ec2-user >> $LOG
   echo "Ensure service starts on every reboot" >> $LOG
-  chkconfig docker on
-
+  chkconfig docker on >> $LOG
 }
 
 # DO ONCE, AND REBOOT
 HAS_REBOOTED=/etc/rebooted
 echo "Checking has rebooted" >> $LOG
 if [ ! -f "$HAS_REBOOTED" ]; then
+  echo "Has not rebooted" >> $LOG
 
   echo "Installing dependencies" >> $LOG
   install_dependencies
 
-  echo "Has not rebooted" >> $LOG
-  touch $HAS_REBOOTED
   echo "Creating Has rebooted file" >> $LOG
-  rm /var/lib/cloud/instances/*/sem/config_scripts_user >> $LOG
+  touch $HAS_REBOOTED
+
   echo "Removing has run user data semaphore" >> $LOG
+  rm /var/lib/cloud/instances/*/sem/config_scripts_user >> $LOG
+
   echo "Rebooting" >> $LOG
   reboot >> $LOG
 fi
@@ -42,5 +43,9 @@ aws ecr get-login-password --region $aws_region | docker login --username AWS --
 
 # pull images to ec2 instance
 echo "Pulling docker image" >> $LOG
-docker pull $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/rssmail:latest >> $LOG
+docker_image=$aws_account_id.dkr.ecr.$aws_region.amazonaws.com/rssmail:latest >> $LOG
+docker pull $docker_image >> $LOG
+
+echo "Setting up container to run every time we restart, unless we specifically stop it." >> $LOG
+docker run -d --restart unless-stopped $docker_image >> $LOG
 
