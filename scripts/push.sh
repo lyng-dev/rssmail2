7264e3aaab72db39b86a1b72891a91041b1c6b5e
Steffen Mads Lyng <sm@lyng.dev>
2022-03-20 13:21:43 +0100

    ref: fixed some infrastructure

--- scripts/push.sh
+++ scripts/push.sh
@@ -7,13 +7,13 @@ TAG=$1
   source .env
 
   #upload
-  aws ecr get-login-password --region us-east-1 --profile rssmail | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+  aws ecr get-login-password --region $AWS_REGION --profile $AWS_PROFILE | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
   docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/rssmail:$TAG
 
   #remove current tag
-  aws ecr batch-delete-image --repository-name rssmail --profile rssmail --image-ids imageTag=latest || true #allow failure, because there might not be a latest
+  aws ecr batch-delete-image --repository-name rssmail --profile $AWS_PROFILE --image-ids imageTag=latest --region $AWS_REGION || true #allow failure, because there might not be a latest
 
   #retag with latest
-  MANIFEST=$(aws ecr batch-get-image --profile rssmail --repository-name rssmail --image-ids imageTag=$TAG --output json | jq --raw-output '.images[0].imageManifest')
-  aws ecr put-image --profile rssmail --repository-name rssmail --image-tag latest --image-manifest "$MANIFEST"
+  MANIFEST=$(aws ecr batch-get-image --profile $AWS_PROFILE --repository-name rssmail --image-ids imageTag=$TAG --region $AWS_REGION --output json | jq --raw-output '.images[0].imageManifest')
+  aws ecr put-image --profile $AWS_PROFILE --repository-name rssmail --image-tag latest --region $AWS_REGION --image-manifest "$MANIFEST"
 )
